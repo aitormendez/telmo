5ec5bcdf803d2b082b2eccb6408a6cc5b545a4dd
aitor <aitor@e451.net>
2019-03-14 21:07:27 +0100

    mundo: desplazar telmo frenando + movimiento patas

--- web/app/themes/tlm/resources/assets/scripts/routes/mundo.js
+++ web/app/themes/tlm/resources/assets/scripts/routes/mundo.js
@@ -32,7 +32,11 @@ export default {
       p.draw = function() {
         p.background(230);
         telmo.show();
-        telmo.walk();
+
+        if (telmo.walking == true) {
+          telmo.walk();
+          telmo.muevePatas();
+        }
       };
 
       p.windowResized =function() {
@@ -43,7 +47,7 @@ export default {
       }
 
       p.mouseClicked = function() {
-        telmo.setDestino(p.mouseX)
+        telmo.setDestino(p.mouseX);
       }
 
       class Persona {
@@ -52,29 +56,101 @@ export default {
           this.x = pX;
           this.y = pY;
           this.destino = this.x
-          // this.ancho = p.width / 10;
           this.cuerpo = cuerpo;
           this.pataIzq = pataIzq;
           this.pataDer = pataDer;
+          this.pataArriba = this.y + 36;
+          this.pataAbajo = this.y + 44;
+          this.walking = false;
+          this.pataPosQuieto = this.y + 40;
+          this.pataPosIzq = this.pataPosQuieto;
+          this.pataPosder = this.pataPosQuieto;
+          this.pataPosIndicadorDer = 'arriba';
+          this.pataPosIndicadorIzq = 'abajo';
+          this.countIzq = 0;
+          this.countDer = 0;
+          this.destino = undefined;
         }
 
         show() {
-          p.image(this.pataIzq, this.x -25, this.y + 40);
-          p.image(this.pataDer, this.x + 5, this.y + 40);
+          if (this.walking == true) {
+            p.image(this.pataIzq, this.x -25, this.pataPosIzq);
+            p.image(this.pataDer, this.x + 5, this.pataPosder);
+          } else {
+            p.image(this.pataIzq, this.x -25, this.pataPosQuieto);
+            p.image(this.pataDer, this.x + 5, this.pataPosQuieto);
+          }
+
           p.image(this.cuerpo, this.x, this.y);
         }
 
         setDestino(destino) {
           this.destino = destino;
+          this.setWalking();
+        }
+
+        muevePatas() {
+
+          // Right leg
+
+          if (this.pataPosIndicadorDer === 'arriba') {
+              this.pataPosder = this.pataArriba;
+              this.countDer++;
+              if (this.countDer === 10) {
+                this.pataPosIndicadorDer = 'abajo';
+                this.countDer = 0;
+              }
+          } else if (this.pataPosIndicadorDer === 'abajo') {
+            this.pataPosder = this.pataAbajo;
+            this.countDer++;
+            if (this.countDer === 10) {
+              this.pataPosIndicadorDer = 'arriba';
+              this.countDer = 0;
+            }
+          }
+
+          // Left leg
+
+          if (this.pataPosIndicadorIzq === 'arriba') {
+              this.pataPosIzq = this.pataArriba;
+              this.countIzq++;
+              if (this.countIzq === 10) {
+                this.pataPosIndicadorIzq = 'abajo';
+                this.countIzq = 0;
+              }
+          } else if (this.pataPosIndicadorIzq === 'abajo') {
+            this.pataPosIzq = this.pataAbajo;
+            this.countIzq++;
+            if (this.countIzq === 10) {
+              this.pataPosIndicadorIzq = 'arriba';
+              this.countIzq = 0;
+            }
+          }
+        } // muevePatas
+
+        setWalking() {
+          if (this.destino != undefined) {
+            this.walking = true;
+          } else {
+            this.walking = false;
+          }
         }
 
         walk() {
-          if (this.destino > this.x + 2) {
-            this.x = this.x + 2;
-          } else if (this.destino < this.x - 2) {
-            this.x = this.x - 2;
+          if (this.destino > this.x + 50) {
+            this.x = this.x + 3;
+          } else if (this.destino < this.x + 50 && this.destino > this.x + 2) {
+            this.x = this.x + (this.destino - this.x) / 20;
+          } else if (this.destino < this.x - 50) {
+            this.x = this.x - 3;
+          } else if (this.destino > this.x - 50 && this.destino < this.x - 3) {
+            this.x = this.x - (-this.destino + this.x) / 20;
+          } else {
+            this.destino = undefined;
+            this.walking = false;
           }
         }
+
 
       } // class persona
 
