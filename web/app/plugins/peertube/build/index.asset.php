bf508779cac12666f38f610db2c36fd136a34e28
aitor <aitor@e451.net>
2021-01-24 22:12:12 +0100

    peertube plugin mío

---
+++ web/app/plugins/peertube/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c3065006daa808fd447b8d7d322b603c');
