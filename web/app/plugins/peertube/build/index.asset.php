e8d88fdfd02146cef5d21ee0bce3c2f0ea2b49b6
aitor <aitor@e451.net>
2021-01-30 17:05:59 +0100

    olvidar peertube

--- web/app/plugins/peertube/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6bdbe427bfda03a638473444659f894b');
